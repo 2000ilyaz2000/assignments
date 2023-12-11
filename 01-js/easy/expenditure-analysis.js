9da1cf2e2bd76486552d1f87c8d7f6b0192d9fc7
2000ilyaz2000 <141390157+2000ilyaz2000@users.noreply.github.com>
2023-12-11 11:58:17 +0530

    Update expenditure-analysis.js
    easy_assign_done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -1,12 +1,24 @@
 /*
-  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
-  Transaction - an object like { itemName, category, price, timestamp }.
-  Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function isAnagram(str1, str2) {
+  let isAnagram = true;
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+    if (str1.length !== str2.length) {
+    isAnagram = false;
+    return isAnagram;
+  }
+  for (let i = 0; i < str1.length; i++) {
+    if (str2.indexOf(str1[i]) === -1) {
+      isAnagram = false;
+      return isAnagram;
+    }
+  }
+  return isAnagram;
 }
 
-module.exports = calculateTotalSpentByCategory;
+module.exports = isAnagram;
