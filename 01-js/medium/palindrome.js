a7d4b9fd0abe4cd2c47a45e6833dc27daa34070d
2000ilyaz2000 <141390157+2000ilyaz2000@users.noreply.github.com>
2023-12-11 14:56:55 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,21 @@
 */
 
 function isPalindrome(str) {
+  let str1="";
+if(str.length==1){
   return true;
+}
+for (let i = 0; i < str.length; i++) { 
+  const c = str.charAt(i); 
+  if (c.match(/[a-zA-Z0-9]/)) 
+    str1 += c.toLowerCase(); 
+} 
+
+let str3=str1.split("").reverse().join("");
+if(str1.toLowerCase()==str3.toLowerCase()){
+  return true;
+}
+return false;
 }
 
 module.exports = isPalindrome;
